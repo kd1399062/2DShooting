8a1a4f2f0a8b2bda9530ca98209c9dba76df83e8
kd1399062 <kd1399062@st.kobedenshi.ac.jp>
2026-05-13 04:12:26 +0900

    HPバー表示
    HPバー画像を表示、まだ表示するだけ

--- STG/Src/Application/Scene/GameScene/UI/UIManager.cpp
+++ STG/Src/Application/Scene/GameScene/UI/UIManager.cpp
@@ -3,23 +3,28 @@
 
 void C_UIManager::Init()
 {
-	m_tex.Load("Texture/Player/Player.png");
-	m_size = { 64,64 };
-	m_pos = { 100,100 };
+	// HPバー
+	m_HpFrameTex.Load("Texture/UI/HpBarFrame.png");
+	m_HpTex.Load("Texture/UI/HpBar.png");
+	m_HpSize = { 96,24 };	// サイズ
+	m_HpPos = { -300,325 };	// 座標
 }
 
 void C_UIManager::Update()
 {
 	//==================== 行列 ====================
-	m_scaleMat = Math::Matrix::CreateScale(m_scaleX, 1.0f, 1.0f);
-	m_transMat = Math::Matrix::CreateTranslation(m_pos.x, m_pos.y, 0);
-	m_mat = m_scaleMat * m_transMat;
+	// HPバー
+	m_scaleMat = Math::Matrix::CreateScale(5.0f, 1.0f, 1.0f);
+	m_transMat = Math::Matrix::CreateTranslation(m_HpPos.x, m_HpPos.y, 0);
+	m_HpMat = m_scaleMat * m_transMat;
 }
 
 void C_UIManager::Draw()
 {
-	SHADER.m_spriteShader.SetMatrix(m_mat);
-	SHADER.m_spriteShader.DrawTex(&m_tex, Math::Rectangle(0, 0, m_size.x, m_size.y), 1.0f);
+	// HPバー
+	SHADER.m_spriteShader.SetMatrix(m_HpMat);
+	SHADER.m_spriteShader.DrawTex(&m_HpFrameTex, Math::Rectangle(0, 0, m_HpSize.x, m_HpSize.y), 1.0f);
+	SHADER.m_spriteShader.DrawTex(&m_HpTex, Math::Rectangle(0, 0, m_HpSize.x, m_HpSize.y), 1.0f);
 }
 
 void C_UIManager::Release()
--- STG/Src/Application/Scene/GameScene/UI/UIManager.h
+++ STG/Src/Application/Scene/GameScene/UI/UIManager.h
@@ -18,8 +18,6 @@ public:
 private:
 	C_GameScene* m_owner = nullptr;
 
-	KdTexture		m_tex;					// テクスチャ
-	Math::Vector2	m_pos = { 0,0 };		// 座標
 	Math::Vector2	m_scroll = { 0,0 };		// スクロール量
 	Math::Vector2	m_move = { 0,0 };		// 移動量
 	Math::Vector2	m_dir = { 0,0 };		// 方向
@@ -28,5 +26,12 @@ private:
 	Math::Matrix    m_transMat;				// 移動行列
 	Math::Matrix    m_rotasionMat;			// 移動行列
 	Math::Matrix    m_mat;					// 合成行列
-	float           m_scaleX = 1.0f;			// 拡大率
+	float           m_scaleX = 1.0f;		// 拡大率
+
+	// HPバー
+	KdTexture		m_HpFrameTex;					// HPバー枠テクスチャ
+	KdTexture		m_HpTex;						// HPバーテクスチャ
+	Math::Matrix    m_HpMat;						// 合成行列
+	Math::Vector2	m_HpSize		= { 96,24 };	// サイズ
+	Math::Vector2	m_HpPos			= { 100,100 };	// 座標
 };
