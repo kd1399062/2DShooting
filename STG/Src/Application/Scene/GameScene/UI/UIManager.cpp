a7797ef56c9aaf10ce549e1d785774c2a8691afd
kd1399062 <kd1399062@st.kobedenshi.ac.jp>
2026-05-13 01:38:38 +0900

    UIManagerクラス作成
    GamaScene/UI/UIManagerクラス
    中身はまだない

---
+++ STG/Src/Application/Scene/GameScene/UI/UIManager.cpp
@@ -0,0 +1,17 @@
+#include "UIManager.h"
+
+void C_UIManager::Init()
+{
+}
+
+void C_UIManager::Draw()
+{
+}
+
+void C_UIManager::Update()
+{
+}
+
+void C_UIManager::Release()
+{
+}
---
+++ STG/Src/Application/Scene/GameScene/UI/UIManager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class C_UIManager
+{
+public:
+
+	void Init();
+	void Draw();
+	void Update();
+	void Release();
+
+private:
+	C_UIManager() {}
+	~C_UIManager() {}
+};
